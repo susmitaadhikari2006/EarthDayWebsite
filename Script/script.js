b0031e1747181a36afaf88d06af4ac75f5cfe5ae
Susmita Adhikari <susmitaadhikari2006@gmail.com>
2023-04-21 14:08:42 -0400

    add images next

--- Script/script.js
+++ Script/script.js
@@ -94,8 +94,43 @@ target.src = fr.result;
 
 }
 
-function putImage() {
-  var src = document.getElementById("select_image");
-  var target = document.getElementsByClassName("target");
-  showImage(src, target[0]);
-}
+var slider = document.getElementById("slider");
+var sliderWidth = slider.offsetWidth;
+var slideList = document.getElementById("slideWrap");
+var count = 1;
+var items = slideList.querySelectorAll("li").length;
+var prev = document.getElementById("prev");
+var next = document.getElementById("next");
+
+window.addEventListener('resize', function() {
+  sliderWidth = slider.offsetWidth;
+});
+var prevSlide = function() {
+  if(count > 1) {
+    count = count - 2;
+    slideList.style.left = "-" + count * sliderWidth + "px";
+    count++;
+  }
+
+  else if(count = 1) {
+    count = items - 1;
+    slideList.style.left = "-" + count * sliderWidth + "px";
+    count++;
+  }
+};
+var nextSlide = function() {
+  if(count < items) {
+    slideList.style.left = "-" + count * sliderWidth + "px";
+    count++;
+  }
+  else if(count = items) {
+    slideList.style.left = "0px";
+    count = 1;
+  }
+};
+setInterval(function() {
+  nextSlide()
+}, 5000);
+window.onload = function() {
+  responsiveSlider();  
+  }
